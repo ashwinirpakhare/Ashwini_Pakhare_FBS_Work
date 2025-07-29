8bcaad86fea35472075f6978408519937020abef
ashwinirpakhare <ashwinipakhare555@gmail.com>
2025-07-29 20:18:52 +0530

    Add files via upload

---
+++ Assignment1/vowel.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	char a,e,i,o,u,s;
+	char character=o; 
+	
+   if(character=='a' && character=='e' && character=='i' &&  character=='o'&& character=='u')
+   {
+   	printf("character is vowels");
+   	
+	   }	
+else
+{
+	printf("character is consonant");
+}
+}
